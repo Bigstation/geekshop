b7be21818ef00e3c181a08de233bd1dd11bc9069
bigstation <av@unetg.ru>
2021-01-24 13:51:22 +0500

    init

---
+++ mainapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+# контроллер = функция
+# MVC = Model View Controller
+# MVT = Model View Template - в Джанго
+# вьюха (view) = контроллер = функция
+
+def index(request):
+    """Разная логика"""
+    return render(request, 'mainapp/index.html')
+
+def products(request):
+    return render(request, 'mainapp/products.html')
