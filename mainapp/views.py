4f9d1e11a1e16219904830c390b8fb755dc85b90
bigstation <av@unetg.ru>
2021-02-06 07:06:14 +0500

    remake master before lesson4

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 import datetime
 
-from mainapp.models import ProductCategory, Product
+from mainapp.models import Product, ProductCategory
 
 # Create your views here.
 # контроллер = функция
@@ -11,8 +11,6 @@ from mainapp.models import ProductCategory, Product
 # вьюха (view) = контроллер = функция
 
 def index(request):
-    """Разная логика"""
-    """Всяко разно"""
     return render(request, 'mainapp/index.html')
 
 def products(request):
@@ -20,16 +18,5 @@ def products(request):
         'data': datetime.date.today(),
         'products': Product.objects.all(),
         'categories': ProductCategory.objects.all(),
-
-
-
-        #     {'img': '/static/vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png',
-        #      'name': 'Темно-синие широкие строгие брюки ASOS DESIGN',
-        #      'price': '2 890,00 руб.',
-        #      'material': 'Легкая эластичная ткань сирсакер Фактурная ткань.',
-        #      }
-        # ],
-
-        # 'categories': ['Новинки', 'Одежда', 'Обувь', 'Аксесуары', 'Подарки']
     }
     return render(request, 'mainapp/products.html', context)
