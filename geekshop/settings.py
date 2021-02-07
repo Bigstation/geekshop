f654fc05727cbcebeb1fa494b57ea4fbdf798bec
bigstation <av@unetg.ru>
2021-02-08 03:16:27 +0500

    lesson5

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'mainapp',
     'authapp',
+    'basket',
 ]
 
 MIDDLEWARE = [
@@ -110,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'ru-ru'
 
 # TIME_ZONE = 'UTC'
-TIME_ZONE = 'Europe/Ekaterinburg'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
